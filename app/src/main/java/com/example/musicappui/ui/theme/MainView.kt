56a922d520a88ef387b2fe4935d3ad57cba41910
SiD19s <sid1992002@gmail.com>
2024-04-29 18:08:18 +0530

    Drawer Items setup

--- app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
+++ app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
@@ -1,6 +1,11 @@
 package com.example.musicappui.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -14,6 +19,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.musicappui.Screen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -41,4 +50,30 @@ fun MainView(){
     ) {
         Text("Text", modifier = Modifier.padding(it))
     }
+}
+
+@Composable
+fun DrawerItem(
+    selected:Boolean,
+    item: Screen.DrawerScreen,
+    onDrawerItemClicked: () -> Unit
+) {
+    val background = if(selected) Color.DarkGray else Color.White
+    Row(Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .clickable {
+                onDrawerItemClicked()
+            })
+    {
+        androidx.compose.material.Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(text = item.dTitle, style = MaterialTheme.typography.h5)
+
+    }
+
 }
