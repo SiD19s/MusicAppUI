1dad16de26c104115771b3a28e185180e5467c8d
SiD19s <sid1992002@gmail.com>
2024-05-03 16:35:35 +0530

    Drawer appeared

--- app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
+++ app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -13,16 +16,22 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.musicappui.Screen
+import com.example.musicappui.screensInDrawer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -32,6 +41,18 @@ fun MainView(){
 
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
+
+
+    // Allow us to find out on which screen we are
+    val controller : NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+
+    val title = remember{
+        // change that to currentScreen.title
+        mutableStateOf("")
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -46,7 +67,30 @@ fun MainView(){
                     Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "")
                 }}
             )
+        },
+        scaffoldState =scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)){
+                items(screensInDrawer){
+                    item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if(item.dRoute == "add_account"){
+                            //open dialog
+                        }else{
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+
+                    }
+
+                }
+
+            }
         }
+
     ) {
         Text("Text", modifier = Modifier.padding(it))
     }
@@ -59,15 +103,16 @@ fun DrawerItem(
     onDrawerItemClicked: () -> Unit
 ) {
     val background = if(selected) Color.DarkGray else Color.White
-    Row(Modifier
+    Row(
+        Modifier
             .fillMaxWidth()
-            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .padding(horizontal = 8.dp, vertical = 10.dp)
             .background(background)
             .clickable {
                 onDrawerItemClicked()
             })
     {
-        androidx.compose.material.Icon(
+        Icon(
             painter = painterResource(id = item.icon),
             contentDescription = item.dTitle,
             Modifier.padding(end = 8.dp, top = 4.dp)
